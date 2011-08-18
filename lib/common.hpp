6fccecb11df8b8e3ad2f6c2282003d075cb1cce6
toshihoge <toshihoge@example.com>
2011-08-19 02:10:33 +0900

    Start writing common.hpp. It will be contain include files, typedefs and macros.

---
+++ lib/common.hpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+typedef vector<int> vi;
+
+#define DL cerr<<"Debug: "<<__LINE__<<endl
+#define DUMP(x) cerr<<#x<<" = "<<(x)<<" (@"<<__LINE__<<")"<<endl
+#define FOR(i,a,b) for(int i=(a);i<(int)(b);i++)
+#define REP(i,n) FOR(i,0,n)
+
