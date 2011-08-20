bc43c9e51a07db0db695d8a2a0674bfde8d616ea
toshihoge <toshihoge@example.com>
2011-08-20 23:43:03 +0900

    Rename from common.hpp to template.cpp. Add main method. Add separators.

---
+++ lib/template.cpp
@@ -0,0 +1,35 @@
+//--------------------------------------
+// Begin common header
+//--------------------------------------
+#include<cmath>
+#include<complex>
+#include<cstdio>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<set>
+#include<sstream>
+#include<vector>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef complex<double> cd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+#define DL cerr<<"Debug: "<<__LINE__<<endl
+#define DUMP(x) cerr<<#x<<" = "<<(x)<<" (@"<<__LINE__<<")"<<endl
+#define HAS(x,y) ((x).find(y)!=(x).end())
+#define EACH(i,c) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); i++)
+#define FOR(i,a,b) for(int i=(a);i<(int)(b);i++)
+#define REP(i,n) FOR(i,0,n)
+#define WT while(true)
+
+//--------------------------------------
+// End common header
+//--------------------------------------
+
+int main(void) {
+  
+}
