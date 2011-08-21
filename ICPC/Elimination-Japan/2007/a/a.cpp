735d80af55a4b2c180a9bfe1d7a56b7bbda3f840
toshihoge <toshihoge@example.com>
2011-08-21 16:59:25 +0900

    Add solutions for 2007 Japan elimination contest Problem A.

---
+++ ICPC/Elimination-Japan/2007/a/a.cpp
@@ -0,0 +1,55 @@
+//--------------------------------------
+// Begin common header
+//--------------------------------------
+#include<algorithm>
+#include<cmath>
+#include<complex>
+#include<cstdio>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<set>
+#include<sstream>
+#include<vector>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef complex<double> cd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<double> vd;
+
+#define DL cerr<<"Debug: "<<__LINE__<<endl
+#define DUMP(x) cerr<<#x<<" = "<<(x)<<" (@"<<__LINE__<<")"<<endl
+#define HAS(x,y) ((x).find(y)!=(x).end())
+#define EACH(i,c) for(typeof((c).begin()) i=(c).begin(); i!=(c).end(); i++)
+#define FOR(i,a,b) for(int i=(a);i<(int)(b);i++)
+#define REP(i,n) FOR(i,0,n)
+#define SORT(c) sort((c).begin(), (c).end())
+#define WT while(true)
+
+//--------------------------------------
+// End common header
+//--------------------------------------
+
+int main(void) {
+  WT {
+    int n;
+    cin >> n;
+    if (!n) {
+      break;
+    }
+    int sum = 0;
+    int mins = 10000;
+    int maxs = 0;
+    REP(i,n) {
+      int score;
+      cin >> score;
+      sum += score;
+      mins = min(mins, score);
+      maxs = max(maxs, score);
+    }
+    cout << (sum - mins - maxs) / (n-2) << endl;
+  }
+}
