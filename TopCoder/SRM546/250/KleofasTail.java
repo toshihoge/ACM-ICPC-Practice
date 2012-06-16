f762df54c73439cd89f09a553d4ab23afeaf5ca4
Hidetoshi Muta <mutahidetoshi@gmail.com>
2012-06-17 04:17:09 +0900

    Add SRM546/250 solution. This code was written DURING the contest.

---
+++ TopCoder/SRM546/250/KleofasTail.java
@@ -0,0 +1,165 @@
+import java.util.*;
+public class KleofasTail {
+  public long countGoodSequences(long K, long A, long B) {
+    if (K == 0) {
+      return B - A + 1;
+    }
+    if (K % 2 == 1) {
+      if (A <= K && K <= B) {
+        return 1+countGoodSequences(2*K, A, B);
+      }
+      return countGoodSequences(2*K, A, B);
+    }
+    long res = 0;
+    for (int i = 0; true; i++) {
+      long f = 1L << i;
+      long min = K*f;
+      if (min > B){
+        break;
+      }
+      long max = K*f + 2*f - 1;
+      long rangemin = Math.max(A, min);
+      long rangemax = Math.min(B, max);
+      if (rangemin <= rangemax) {
+        res += rangemax - rangemin + 1;
+      }
+    }
+    return res;
+  }
+
+// BEGIN CUT HERE
+    public static void main(String[] args) {
+        try {
+            eq(0,(new KleofasTail()).countGoodSequences(3L, 4L, 8L),2L);
+            eq(1,(new KleofasTail()).countGoodSequences(1L, 23457L, 123456L),100000L);
+            eq(2,(new KleofasTail()).countGoodSequences(1234567890123456L, 10L, 1000000L),0L);
+            eq(3,(new KleofasTail()).countGoodSequences(0L, 0L, 2L),3L);
+            eq(4,(new KleofasTail()).countGoodSequences(2L, 3L, 3L),1L);
+            eq(5,(new KleofasTail()).countGoodSequences(13L, 12345L, 67890123L),8387584L);
+        } catch( Exception exx) {
+            System.err.println(exx);
+            exx.printStackTrace(System.err);
+        }
+    }
+    private static void eq( int n, int a, int b ) {
+        if ( a==b )
+            System.err.println("Case "+n+" passed.");
+        else
+            System.err.println("Case "+n+" failed: expected "+b+", received "+a+".");
+    }
+    private static void eq( int n, char a, char b ) {
+        if ( a==b )
+            System.err.println("Case "+n+" passed.");
+        else
+            System.err.println("Case "+n+" failed: expected '"+b+"', received '"+a+"'.");
+    }
+    private static void eq( int n, long a, long b ) {
+        if ( a==b )
+            System.err.println("Case "+n+" passed.");
+        else
+            System.err.println("Case "+n+" failed: expected \""+b+"L, received "+a+"L.");
+    }
+    private static void eq( int n, boolean a, boolean b ) {
+        if ( a==b )
+            System.err.println("Case "+n+" passed.");
+        else
+            System.err.println("Case "+n+" failed: expected "+b+", received "+a+".");
+    }
+    private static void eq( int n, String a, String b ) {
+        if ( a != null && a.equals(b) )
+            System.err.println("Case "+n+" passed.");
+        else
+            System.err.println("Case "+n+" failed: expected \""+b+"\", received \""+a+"\".");
+    }
+    private static void eq(int n, double a, double b) {
+        if (Math.abs(a - b) < 1e-9)
+            System.err.println("Case "+n+" passed.");
+        else
+            System.err.println("Case "+n+" failed: expected \""+b+"\", received \""+a+"\".");
+    }
+    private static void eq( int n, int[] a, int[] b ) {
+        if ( a.length != b.length ) {
+            System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
+            return;
+        }
+        for ( int i= 0; i < a.length; i++)
+            if ( a[i] != b[i] ) {
+                System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
+                print( b );
+                print( a );
+                return;
+            }
+        System.err.println("Case "+n+" passed.");
+    }
+    private static void eq( int n, long[] a, long[] b ) {
+        if ( a.length != b.length ) {
+            System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
+            return;
+        }
+        for ( int i= 0; i < a.length; i++ )
+            if ( a[i] != b[i] ) {
+                System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
+                print( b );
+                print( a );
+                return;
+            }
+        System.err.println("Case "+n+" passed.");
+    }
+    private static void eq( int n, String[] a, String[] b ) {
+        if ( a.length != b.length) {
+            System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
+            return;
+        }
+        for ( int i= 0; i < a.length; i++ )
+            if( !a[i].equals( b[i])) {
+                System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
+                print( b );
+                print( a );
+                return;
+            }
+        System.err.println("Case "+n+" passed.");
+    }
+    private static void print( int a ) {
+        System.err.print(a+" ");
+    }
+    private static void print( long a ) {
+        System.err.print(a+"L ");
+    }
+    private static void print( String s ) {
+        System.err.print("\""+s+"\" ");
+    }
+    private static void print( int[] rs ) {
+        if ( rs == null) return;
+        System.err.print('{');
+        for ( int i= 0; i < rs.length; i++ ) {
+            System.err.print(rs[i]);
+            if ( i != rs.length-1 )
+                System.err.print(", ");
+        }
+        System.err.println('}');
+    }
+    private static void print( long[] rs) {
+        if ( rs == null ) return;
+        System.err.print('{');
+        for ( int i= 0; i < rs.length; i++ ) {
+            System.err.print(rs[i]);
+            if ( i != rs.length-1 )
+                System.err.print(", ");
+        }
+        System.err.println('}');
+    }
+    private static void print( String[] rs ) {
+        if ( rs == null ) return;
+        System.err.print('{');
+        for ( int i= 0; i < rs.length; i++ ) {
+            System.err.print( "\""+rs[i]+"\"" );
+            if( i != rs.length-1)
+                System.err.print(", ");
+        }
+        System.err.println('}');
+    }
+    private static void nl() {
+        System.err.println();
+    }
+// END CUT HERE
+}
