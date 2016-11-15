603ab25df5dc2338d337636a069f82bfb881a6bb
Hidetoshi Muta <mutahidetoshi@gmail.com>
2016-11-15 13:24:33 -0800

    Add arc015/a.

---
+++ atcoder/arc015/a/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    double n = s.nextDouble();
+    System.out.println(1.8*n + 32);
+  }
+}
