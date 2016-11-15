9f283dc5d211bbc86e8482ccd7baac12f01076c0
Hidetoshi Muta <mutahidetoshi@gmail.com>
2016-11-15 09:08:08 -0800

    Add arc014/a.

---
+++ atcoder/arc014/a/Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println(s.nextInt() % 2 == 1 ? "Red" : "Blue");
+  }
+}
