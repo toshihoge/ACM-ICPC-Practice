a16941068fba71ece4561f2766cad5f054cf609b
Hidetoshi Muta <mutahidetoshi@gmail.com>
2016-11-15 17:08:44 -0800

    Add arc016/a.

---
+++ atcoder/arc016/a/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.math.*;
+public class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    int m = s.nextInt();
+    System.out.println(m == 1 ? 2 : 1);
+  }
+}
